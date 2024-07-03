8a963fca5608b94638f3db913f96e3756549c02d
dah <daun792@gmail.com>
2024-07-04 04:10:51 +0900

    modify: 폴더 및 코드 정리

--- Assets/02. Scripts/Map/DisappearTile.cs
+++ Assets/02. Scripts/Map/DisappearTile.cs
@@ -6,14 +6,18 @@ using DG.Tweening;
 public class DisappearTile : MonoBehaviour
 {
     private SpriteRenderer sprite;
-    private BoxCollider2D collision;
 
     private readonly float delay = 1f;
     private bool isPlayerOnTile = false;
 
-    private void OnCollisionEnter2D(Collision2D _collision)
+    void Start()
     {
-        if (_collision.gameObject.CompareTag("Player") && !isPlayerOnTile)
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !isPlayerOnTile)
         {
             isPlayerOnTile = true;
 
@@ -21,11 +25,5 @@ public class DisappearTile : MonoBehaviour
             sequence.Append(sprite.DOFade(0f, delay).SetEase(Ease.Linear))
                 .OnComplete(() => gameObject.SetActive(false));
         }
-    }
-
-    void Start()
-    {
-        sprite = GetComponent<SpriteRenderer>();
-        collision = GetComponent<BoxCollider2D>();
     }
 }
