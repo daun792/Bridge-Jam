4f7cd1e988a88ce233989b3a7aff9ab06c5a842c
dah <daun792@gmail.com>
2024-07-05 00:07:45 +0900

    feat: StageBase 기초 기능 구현

--- Assets/02. Scripts/Map/StageBase.cs
+++ Assets/02. Scripts/Map/StageBase.cs
@@ -4,8 +4,56 @@ using UnityEngine;
 
 public class StageBase : MonoBehaviour
 {
+    struct StageComposition
+    {
+        public readonly GameObject stageParent;
+        public readonly Transform startPosition;
+        public readonly SpriteRenderer[] disappearTiles;
+    }
+
+    [SerializeField] StageComposition cleanStage;
+    [SerializeField] StageComposition drugStage;
+
+    private bool isClean = true;
+    private StageComposition currStage;
+
+    public int StageIndex { get; protected set; }
+
+    public void SetStage(bool _isClean)
+    {
+        isClean = _isClean;
+        currStage = _isClean ? cleanStage : drugStage;
+
+        currStage.stageParent.SetActive(true);
+    }
+
+    public Vector3 GetStartPosition() => isClean switch
+    {
+        true => cleanStage.startPosition.position,
+        false => drugStage.startPosition.position
+    };
+
+    public void ResetStage()
+    {
+        if (currStage.disappearTiles.Length > 0)
+        {
+            var disappearTiles = currStage.disappearTiles;
+            var fullAlpha = new Color(0f, 0f, 0f, 1f);
+
+            foreach (var tile in disappearTiles)
+            {
+                tile.gameObject.SetActive(true);
+                tile.color += fullAlpha;
+            }
+        }
+    }
+
+    public virtual void UseDrug()
+    {
+
+    }
+
     
-    // Start is called before the first frame update
     void Start()
     {
         
